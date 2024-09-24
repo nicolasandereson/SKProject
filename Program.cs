dafdfe8a75a9d3c215f42fce162dd054231332ec
Nicolas Anderson <nicolas-31@outlook.com>
2024-09-24 07:55:38 +0200

    have added my first semantic kernel project

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.SemanticKernel;
+
+var builder = Kernel.CreateBuilder();
+
+
+// your deployment name from earlier step 
+// your API key and endpoint under Resource Management in Azure portal
+builder.AddAzureOpenAIChatCompletion(
+    "your-deployment-name",
+    "your-endpoint",
+    "your-api-key");
+
+var kernel = builder.Build();
+
+var result = await kernel.InvokePromptAsync(
+        "What are autonomous cars?");
+    Console.WriteLine(result);
